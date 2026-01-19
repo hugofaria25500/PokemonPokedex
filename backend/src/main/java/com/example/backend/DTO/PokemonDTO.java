863e8c680bc9ad1104c505937cbeae8a80b5bd9e
Hugo Faria <altran-hfaria@iefp.pt>
2026-01-19 23:25:01 +0000

    defining mvp dto's

---
+++ backend/src/main/java/com/example/backend/DTO/PokemonDTO.java
@@ -0,0 +1,45 @@
+package com.example.backend.DTO;
+
+import java.util.List;
+
+public class PokemonDTO {
+
+    private long id;
+    private String name;
+
+    //https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg
+    private String imageUrl;
+
+    public PokemonDTO() {
+    }
+
+    public PokemonDTO(long id, String name, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl = imageUrl;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
---
+++ backend/src/main/java/com/example/backend/DTO/TypesDTO.java
@@ -0,0 +1,43 @@
+package com.example.backend.DTO;
+
+public class TypesDTO {
+
+    private long id;
+
+    private String name;
+
+    private String imageUrl;
+
+    public TypesDTO() {
+    }
+
+    public TypesDTO(long id, String name, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl = imageUrl;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
