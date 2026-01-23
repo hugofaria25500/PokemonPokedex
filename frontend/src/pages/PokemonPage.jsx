4098048379ca1dd172e713d04b9c86152d25f85f
Hugo Faria <altran-hfaria@iefp.pt>
2026-01-23 17:45:52 +0000

    save changes

--- frontend/src/pages/PokemonPage.jsx
+++ frontend/src/pages/PokemonPage.jsx
@@ -1,17 +1,24 @@
 import { pokemonData } from "../js/data.js";
 import PokemonCard from "../components/PokemonCard";
+import "../css/PokemonPage.css";
 
 function PokemonPage() {
-
   return (
-    <div className="pokemon-grid">
-      {pokemonData.map((pokemon) => (
-        <PokemonCard
-          key={pokemon.id}
-          pokemon={pokemon}
-        />
-      ))}
-    </div>
+      <div>
+        <nav className="navbar-container">
+          <h1>Pokémon Gallery</h1>
+        </nav>
+
+        <div className="pokemon-grid">
+          {pokemonData.map((pokemon) => (
+            <PokemonCard
+              key={pokemon.id}
+              pokemon={pokemon}
+            />
+          ))}
+        </div>
+
+      </div>
   );
 }
 
