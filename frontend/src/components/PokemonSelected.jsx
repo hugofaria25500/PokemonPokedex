7b3a784bc3cbcb1cc3c73e8a3a00c2f7fe1d0119
Hugo Faria <altran-hfaria@iefp.pt>
2026-01-29 01:10:30 +0000

    continue building pokemon selected

--- frontend/src/components/PokemonSelected.jsx
+++ frontend/src/components/PokemonSelected.jsx
@@ -1,44 +1,44 @@
-import "../css/PokemonSelected.css"
+import "../css/PokemonSelected.css";
 
 function PokemonSelected({ pokemon }) {
   return (
     <div className="pokemon-selected-card">
-      <div className="card-base">
         <img className="type-logo" src={pokemon.typeUrl} />
-        <div className="id-box">
-          <p>#{pokemon.id}</p>
+        <div className="card-base">
+
+                <div className="id-box">
+                  <p>#{pokemon.id}</p>
+                </div>
+
+                <div className="frame-box">
+                    <img src="" alt="" />
+                    {/*<img src={pokemon.imageUrl}/>*/}
+                </div>
+
+                <div className="label-box"> 
+                    <p>{pokemon.name}</p>
+                    <div className="type-badge-box">
+                      {pokemon.type.map((type, index) => (
+                        <p key={index} className={`type-badge ${type.toLowerCase()}-type-badge`}>
+                          {type}
+                        </p>))}
+                    </div>
+                </div>
         </div>
-        <div className="frame-box">
-          <img src={pokemon.imageUrl} />
+
+        <div className="about">
+            <h3>About</h3>
         </div>
-        <div className="label-box">
-          <p>{pokemon.name}</p>
-          <div className="type-badge-box">
-            {pokemon.type.map((type, index) => (
-              <p key={index} className={`type-badge ${type.toLowerCase()}-type-badge`}> {type}</p>
-            ))}
-          </div>
+        <div className="base-stats">
+            <h3>Base Stats</h3>
+        </div>
+        <div className="abilities">
+            <h3>Abilities</h3>
+        </div>
+        <div className="evolution-chain">
+            <h3>Evolution Chain</h3>
         </div>
       </div>
-      
-      <div className="minimal-stats">
-        <h2>#{pokemon.id}</h2>
-        <h2>{pokemon.name}</h2>
-        <h2>{pokemon.type}</h2>
-      </div>
-      <div className="about">
-        <h3>About</h3>
-      </div>
-      <div className="base-stats">
-        <h3>Base Stats</h3>
-      </div>
-      <div className="abilities">
-        <h3>Abilities</h3>
-      </div>
-      <div className="evolution-chain">
-        <h3>Evolution Chain</h3>
-      </div>
-    </div>
   );
 }
 
