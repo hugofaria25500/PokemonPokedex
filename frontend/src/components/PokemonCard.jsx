0268c673d929be8dbb02d001a7511b3558388764
Hugo Faria <altran-hfaria@iefp.pt>
2026-02-25 15:52:57 +0000

    correcting the types property in order to add dinamic type background color

--- frontend/src/components/PokemonCard.jsx
+++ frontend/src/components/PokemonCard.jsx
@@ -1,11 +1,12 @@
 import "../css/PokemonTypes.css"
 import "../css/PokemonCard.css"
+import { typeUrl } from "../js/typesImage.js";
 
 function PokemonCard({ pokemon }) {
     {/*POKEMON CARD BUILDING*/}
     return (
         <div className="pokemon-card">
-            <img className="type-logo" src={pokemon.typeUrl} />
+            <img className="type-logo" src={typeUrl[pokemon.types[0].toLowerCase()]} />
             <div className="id-box">
                 <p>#{pokemon.id}</p>
             </div>
@@ -15,7 +16,7 @@ function PokemonCard({ pokemon }) {
             <div className="label-box">
                 <p>{pokemon.name}</p>
                 <div className="type-badge-box">
-                    {pokemon.type.map((type, index) => (
+                    {pokemon.types.map((type, index) => (
                       <p key={index} className={`type-badge ${type.toLowerCase()}-type-badge`}> {type}</p>
                     ))}
                 </div>
