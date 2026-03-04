a4a3c92d524c249e0211e17426d770ac0a67ee50
Hugo Faria <hugofaria25500@gmail.com>
2026-03-04 14:17:14 +0000

    refactoring the pokemonService

--- backend/src/main/java/com/example/backend/service/PokemonService.java
+++ backend/src/main/java/com/example/backend/service/PokemonService.java
@@ -1,101 +1,12 @@
 package com.example.backend.service;
 
-import com.example.backend.client.PokemonClient;
-import com.example.backend.client.response.*;
-import com.example.backend.dto.*;
-import com.example.backend.exception.PokemonNotFoundException;
-import com.example.backend.mapper.PokemonMapper;
-import com.example.backend.utils.PokemonUtils;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.stereotype.Service;
+import com.example.backend.dto.PokemonDTO;
+import com.example.backend.dto.PokemonDetailDTO;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class PokemonService {
-    private final PokemonClient client;
-
-    private final PokemonMapper mapper;
-
-    public PokemonService(PokemonClient client, PokemonMapper mapper) {
-        this.client = client;
-        this.mapper = mapper;
-    }
-
-    @Cacheable("pokemon")
-    public PokemonDTO getPokemon(long id) {
-        PokeApiPokemonResponse response = client.getPokemonById(id);
-
-        if (response == null) {
-            throw new PokemonNotFoundException("Pokemon with id " + id + " not found");
-        }
-
-        return mapper.toPokemonDTO(response);
-    }
-
-    @Cacheable("selectedPokemon")
-    public PokemonDetailDTO getPokemonDetails(long id) {
-        PokeApiPokemonDetailResponse response = client.getPokemonDetailsById(id);
-
-        if (response == null) {
-            throw new PokemonNotFoundException("Pokemon with id " + id + " not found");
-        }
-
-        PokeApiPokemonSpecieResponse specieResponse = client.getPokemonSpeciesById(PokemonUtils.extractIdFromUrl(response.getDescription().getUrl()));
-        long evolutionChainId = PokemonUtils.extractIdFromUrl(specieResponse.getEvolutionChain().getEvolutionChainUrl());
-
-
-        List<AbilitiyDTO> abilitiyDTOList = response.getAbilities()
-            .stream()
-            .map(abilityEntry -> {
-
-                PokeApiPokemonAbilitiesResponse abilitiesResponse =
-                        client.getPokemonAbilitiesById(
-                                PokemonUtils.extractIdFromUrl(abilityEntry.getAbility().getUrl())
-                        );
-
-                String name = abilitiesResponse.getName();
-
-                String description = abilitiesResponse.getDescriptions()
-                        .stream()
-                        .filter(entry -> "en".equals(entry.getLanguage().getName()))
-                        .filter(entry -> "platinum".equals(entry.getVersion().getName()))
-                        .map(entry -> entry.getDescription())
-                        .findFirst()
-                        .orElse("No description available");
-
-                return new AbilitiyDTO(name, description);
-            }).toList();
-
-        PokeApiPokemonEvolutionChainResponse evolutionResponse = client.getPokemonEvolutionChainById(evolutionChainId);
-        List<EvolutionChainDTO> evolutionChainDTOList = new ArrayList<>();
-
-        PokemonUtils.buildEvolutionList(evolutionResponse.getChainLink(),new ArrayList<>(),evolutionChainDTOList);
-
-        return mapper.toPokemonDetailDTO(response, specieResponse, abilitiyDTOList, evolutionChainDTOList);
-    }
-
-    @Cacheable("pokemons")
-    public List<PokemonDTO> getFirstPokemons() {
-        PokeApiPokemonListResponse listResponse = client.getFirstPokemons();
-
-        if (listResponse == null) {
-            throw new PokemonNotFoundException("Pokemons not found");
-        }
-
-        return listResponse.getResults()
-                .parallelStream()
-                .map(entry -> {
-
-                    long id = PokemonUtils.extractIdFromUrl(entry.getUrl());
-
-                    PokeApiPokemonResponse response =
-                            client.getPokemonById(id);
-
-                    return mapper.toPokemonDTO(response);
-                })
-                .toList();
-    }
+public interface PokemonService {
+    public PokemonDTO getPokemon(long id);
+    public PokemonDetailDTO getPokemonDetails(long id);
+    public List<PokemonDTO> getFirstPokemons();
 }
-
