1a5ede0b03b9e289aa5a80f7841aae370c6b842c
Hugo Faria <hugofaria25500@gmail.com>
2026-03-13 01:02:45 +0000

    some fixes

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,9 +1,11 @@
+import LogoImage from "../resources/img/icons/pokemon_logo.png";
+
 import "../css/Logo.css"
 
 function Logo () {
     return (
         <div className="logo-container">
-            <img src="" alt="Logo Image" />
+            <img src={LogoImage} alt="Logo Image" />
         </div>
     );
 }
